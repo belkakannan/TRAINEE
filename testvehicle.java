6b1daf8b37e05908514ccc8375e0dae8399f6cdc
Belka K <belk3340@global.chainsys.com>
2022-11-15 10:36:35 +0530

    first commit

---
+++ testvehicle.java
@@ -0,0 +1,20 @@
+package vehicle;
+
+public class testvehicle {
+
+	public static void main(String[] args) {
+		vehicle vehicle1 = new vehicle();
+		vehicle1.vehicleNo = "TN23H5359";
+		vehicle1.vehicleName = "CAR";
+		vehicle1.fuelType = "diesel";
+		vehicle1.millage = 200000;
+		vehicle1.vehiclePrice = 25000000;
+		vehicle1.vehicleModel = 2015;
+		System.out.println("vehicle1.vehicleName: " + vehicle1.vehicleName);
+		System.out.println(" vehicle1.fueltype : " + vehicle1.fuelType);
+		
+		
+
+	}
+
+}
---
+++ vehicle.java
@@ -0,0 +1,16 @@
+package vehicle;
+
+public class vehicle {
+
+	
+		// TODO Auto-generated method stub
+		String vehicleNo ;
+		String vehicleName;
+		String fuelType;
+		int millage;
+		int vehiclePrice;
+		int vehicleModel;
+
+	}
+
+
